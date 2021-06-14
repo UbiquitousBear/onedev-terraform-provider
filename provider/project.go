8126faefba7638174dfce8069825f9e052e3b324
Shamil Nunhuck <shamil@shamil.co.uk>
2021-06-14 12:59:11 +0100

    Using onedev-api

--- provider/project.go
+++
@@ -1,9 +0,0 @@
-package provider
-
-type Project struct {
-	Id                     int    `json:"id,omitempty"`
-	ForkedFromId           int    `json:"forkedFromId,omitempty"`
-	Name                   string `json:"name"`
-	Description            string `json:"description"`
-	IssueManagementEnabled bool   `json:"issueManagementEnabled"`
-}
